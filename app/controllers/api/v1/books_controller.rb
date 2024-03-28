3619b715d11d66e4dab692a8bb037eff4691fc5d
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-28 14:10:40 +0200

    Define edit and update action on books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -9,4 +9,39 @@ class Api::V1::BooksController < ApplicationController
     @book = Book.find(params[:id])
     render json: @book
   end
+
+  def new
+    @book = Book.new
+    render json: @book
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created
+    else
+      render json: @book.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      render json: @book, status: :ok
+    else
+      render json: @book.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :author, :photo)
 end
