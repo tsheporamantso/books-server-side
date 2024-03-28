d2e13c2e59322a22b01bcbcc6eced0996822feda
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-28 13:40:10 +0200

    Add new and create actions on route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index, :show]
+      resources :books, only: [:index, :show, :new, :create]
     end
   end
-  get "up" => "rails/health#show", as: :rails_health_check
 end
