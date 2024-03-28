2f31a96064356d32d3a55417efdca4bb30fe5b85
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-28 13:06:53 +0200

    Add author column on books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_092622) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_105922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_092622) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
