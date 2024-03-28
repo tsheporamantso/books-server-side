4af964a1006e90ce267a30acfd896a4489aebc0e
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-28 13:35:43 +0200

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:index]
+      resources :books, only: [:index, :show]
     end
   end
   get "up" => "rails/health#show", as: :rails_health_check
